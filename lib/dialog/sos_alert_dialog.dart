00c266e5041978e98ff334fbe84e9895b3f6c568
naturenook1 <hariom@naturenookmart.com>
2024-12-12 18:45:12 +0530

    checking git working...

--- lib/dialog/sos_alert_dialog.dart
+++ lib/dialog/sos_alert_dialog.dart
@@ -11,6 +11,7 @@ import 'package:nb_utils/nb_utils.dart';
 import '../screens/navigation_screen.dart';
 
 
+
 /*"-Nxm4aTJ0-gxhXu0FDTh": {
           "Date": "May 13, 2024 at 6:13:24 PM",
           "Emergency": "A user needs emergency service",
